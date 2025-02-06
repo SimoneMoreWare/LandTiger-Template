da4b557b1f5b4287c39a8b6a97283874f886c931
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2025-02-06 10:56:49 +0100

    Update lib_button.c

--- Source/button_EXINT/lib_button.c
+++ Source/button_EXINT/lib_button.c
@@ -7,7 +7,7 @@
  */
 void BUTTON_init(void) {
 
-	LPC_PINCON->PINSEL4    |= (1 << 20);			/* External interrupt 0 pin selection */
+	LPC_PINCON->PINSEL4    |= (1 << 20);		/* External interrupt 0 pin selection */
 	LPC_GPIO2->FIODIR      &= ~(1 << 10);    	/* PORT2.10 defined as input          */
 
 	LPC_PINCON->PINSEL4    |= (1 << 22);     	/* External interrupt 0 pin selection */
@@ -20,11 +20,11 @@ void BUTTON_init(void) {
 
 	
 	NVIC_EnableIRQ(EINT2_IRQn);              	/* enable irq in nvic                 */
-	NVIC_SetPriority(EINT2_IRQn, 1);					/* priority, the lower the better     */
+	NVIC_SetPriority(EINT2_IRQn, 1);		/* priority, the lower the better     */
 	
 	NVIC_EnableIRQ(EINT1_IRQn);             	/* enable irq in nvic                 */
 	NVIC_SetPriority(EINT1_IRQn, 2);				 
 	
-	NVIC_EnableIRQ(EINT0_IRQn);								/* enable irq in nvic                  */
-	NVIC_SetPriority(EINT0_IRQn, 3);					/* decreasing priority	from EINT2->0	 */
+	NVIC_EnableIRQ(EINT0_IRQn);			/* enable irq in nvic                  */
+	NVIC_SetPriority(EINT0_IRQn, 3);		/* decreasing priority	from EINT2->0  */
 }
