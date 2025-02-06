e778bb7576a345dd1b7d9bb7f0d9d260d3fde300
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2025-02-06 10:58:26 +0100

    Update lib_joystick.c

--- Source/joystick/lib_joystick.c
+++ Source/joystick/lib_joystick.c
@@ -23,11 +23,11 @@ void joystick_init(void) {
 	
 	/* joystick dx functionality */
 	LPC_PINCON->PINSEL3 &= ~(3<<24);	//PIN mode GPIO (00b value per P1.28)
-   LPC_GPIO1->FIODIR   &= ~(1<<28);	//P1.28 Input (joysticks on PORT1 defined as Input)
+  	LPC_GPIO1->FIODIR   &= ~(1<<28);	//P1.28 Input (joysticks on PORT1 defined as Input)
 	
 	/* joystick sx functionality */
 	LPC_PINCON->PINSEL3 &= ~(3<<22);	//PIN mode GPIO (00b value per P1.27)
-   LPC_GPIO1->FIODIR   &= ~(1<<27);	//P1.27 Input (joysticks on PORT1 defined as Input) 
+   	LPC_GPIO1->FIODIR   &= ~(1<<27);	//P1.27 Input (joysticks on PORT1 defined as Input) 
 	
 	/* joystick down functionality */
 	LPC_PINCON->PINSEL3 &= ~(3<<20);	//PIN mode GPIO (00b value per P1.26)
@@ -35,6 +35,6 @@ void joystick_init(void) {
 	
 	/* joystick clicked functionality */
 	LPC_PINCON->PINSEL3 &= ~(3<<18);	//PIN mode GPIO (00b value per P1.25)
-  LPC_GPIO1->FIODIR   &= ~(1<<25);	//P1.25 Input (joysticks on PORT1 defined as Input)
+  	LPC_GPIO1->FIODIR   &= ~(1<<25);	//P1.25 Input (joysticks on PORT1 defined as Input)
 
 }
