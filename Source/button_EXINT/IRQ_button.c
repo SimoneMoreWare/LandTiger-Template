e9bd27500175f61c780e8976d785b18e3ec70488
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2025-02-06 10:56:28 +0100

    fix indentation

--- Source/button_EXINT/IRQ_button.c
+++ Source/button_EXINT/IRQ_button.c
@@ -6,7 +6,7 @@
 #include "../timer/timer.h"
 
 
-	/* Variabili Globali Gestione De-Bouncing */
+/* Variabili Globali Gestione De-Bouncing */
 	
 extern int down_0;
 extern int down_1;
@@ -16,27 +16,27 @@ extern int down_2;
 void EINT0_IRQHandler (void)	  	// INT0
 {		
 	down_0 = 1;
-	NVIC_DisableIRQ(EINT0_IRQn);											/* disable Button interrupts		*/
-	LPC_PINCON->PINSEL4    &= ~(1 << 20);     				/* GPIO pin selection 					*/
+	NVIC_DisableIRQ(EINT0_IRQn);						/* disable Button interrupts	*/
+	LPC_PINCON->PINSEL4    &= ~(1 << 20);     				/* GPIO pin selection 		*/
 	
-	LPC_SC->EXTINT &= (1 << 0);     									/* clear pending interrupt      */
+	LPC_SC->EXTINT &= (1 << 0);     					/* clear pending interrupt      */
 }
 
 
 void EINT1_IRQHandler (void)	  	// KEY1	
 {
 	down_1 = 1;
-	NVIC_DisableIRQ(EINT1_IRQn);											/* disable Button interrupts		*/
-	LPC_PINCON->PINSEL4    &= ~(1 << 22);     				/* GPIO pin selection 					*/
+	NVIC_DisableIRQ(EINT1_IRQn);						/* disable Button interrupts	*/
+	LPC_PINCON->PINSEL4    &= ~(1 << 22);     				/* GPIO pin selection 		*/
 	
-	LPC_SC->EXTINT &= (1 << 1);     									/* clear pending interrupt      */
+	LPC_SC->EXTINT &= (1 << 1);     					/* clear pending interrupt      */
 }
 
 void EINT2_IRQHandler (void)	  	// KEY2
 {
 	down_2 = 1;
-	NVIC_DisableIRQ(EINT2_IRQn);											/* disable Button interrupts		*/	
-	LPC_PINCON->PINSEL4    &= ~(1 << 24);    	 				/* GPIO pin selection 					*/
+	NVIC_DisableIRQ(EINT2_IRQn);						/* disable Button interrupts	*/	
+	LPC_PINCON->PINSEL4    &= ~(1 << 24);    	 			/* GPIO pin selection 		*/
 	
-	LPC_SC->EXTINT &= (1 << 2);     									/* clear pending interrupt      */    
+	LPC_SC->EXTINT &= (1 << 2);     					/* clear pending interrupt      */    
 }
